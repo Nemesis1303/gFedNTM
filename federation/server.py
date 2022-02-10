8b28d325fbfa1a677dcaaf8103907e4323ea87bd
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2022-02-10 13:09:30 +0100

    Server updated.

--- federation/server.py
+++ federation/server.py
@@ -16,7 +16,7 @@ import time
 import numpy as np
 
 from federation import federated_pb2, federated_pb2_grpc, federation, federation_client
-
+from utils.auxiliary_functions import get_type_from_string
 
 FORMAT = '[%(asctime)-15s] [%(filename)s] [%(levelname)s] %(message)s'
 logging.basicConfig(format=FORMAT, level='INFO')
@@ -24,6 +24,8 @@ logger = logging.getLogger('server')
 GRPC_TRACE = all
 
 # TODO: Include update client state
+
+
 class FederatedServer(federated_pb2_grpc.FederationServicer):
     """Class that describes the behaviour of the GRPC server to which several clients are connected to create a federation for the joint training of a CTM or ProdLDA model.
 
@@ -35,11 +37,12 @@ class FederatedServer(federated_pb2_grpc.FederationServicer):
         self.current_iteration = -1
         self.id_server = "IDS" + "_" + str(round(time.time()))
 
-    def record_client(self, context, id_client, gradient, 
+    def record_client(self, context, id_client, gradient,
                       current_iter, current_id_msg, num_max_iter):
         """Method to record the communication between a server and one of the clients in the federation.
 
         Args:
+        -----
             * context (AuthMetadataContext): An AuthMetadataContext providing information on the RPC
         """
         def unregister_client():
@@ -59,31 +62,47 @@ class FederatedServer(federated_pb2_grpc.FederationServicer):
         self.federation.connect_waiting(context.peer())
 
     def sendLocalTensor(self, request, context):
-        """[summary]
+        """Sends an ACK response to the client after receiving his local tensor update.
 
         Args:
-            * request (ClientTensorRequest): Request of the client for sending an iteration's gradient
-            * context (AuthMetadataContext): Context of the RPC communication between the client and the server
+        -----
+            * request (ClientTensorRequest): Request of the client for sending an iteration's 
+                                             gradient
+            * context (AuthMetadataContext): Context of the RPC communication between the 
+                                             client and the server
 
         Returns:
-            * ServerReceivedResponse: Server's response to confirm a client that its sent gradient was received
+        --------
+            * ServerReceivedResponse: Server's response to confirm a client that its sent 
+                                      gradient was received
         """
 
         header = federated_pb2.MessageHeader(id_response=self.id_server,
                                              id_to_request=request.header.id_request,
                                              message_type=federated_pb2.MessageType.SERVER_CONFIRM_RECEIVED)
+
+        # Deserialize gradient update from the client and save in the corresponding Client object as numpy array
         dims = tuple([dim.size for dim in request.data.tensor_shape.dim])
+        dtype = get_type_from_string(request.data.dtype)
         deserialized_bytes = np.frombuffer(
-            request.data.tensor_content, dtype=np.float32)
+            request.data.tensor_content, dtype=dtype)
         deserialized_numpy = np.reshape(deserialized_bytes, newshape=dims)
 
+        # Record client in the federation
         self.record_client(context, request.metadata.id_machine, deserialized_numpy,
-                           request.metadata.current_epoch, request.header.id_request, 
+                           request.metadata.current_epoch, request.header.id_request,
                            request.metadata.num_max_epochs)
 
         return federated_pb2.ServerReceivedResponse(header=header)
 
     def can_send_update(self):
+        """Checks the conditions that need to be fullfilled in order to send the average tensor update to the clients.
+
+        Returns:
+        --------
+            * boolean: True if the aggragate update can be sent.
+        """
+
         if len(self.federation.federation_clients) < self.min_num_clients:
             return False
         # TODO: Check with condition can be added here in case not all clients sent the updates
@@ -93,6 +112,19 @@ class FederatedServer(federated_pb2_grpc.FederationServicer):
         return True
 
     def sendAggregatedTensor(self, request, context):
+        """Sends the average update to the correspinding client based on the context of the gRPC channel.
+
+        Args:
+        -----
+            * request (federated_pb2.Empty): Empty protocol buffer coming from the client
+            * context (AuthMetadataContext): Context of the RPC communication between the 
+                                             client and the server
+
+        Returns:
+        --------
+            federated_pb2.ServerAggregatedTensorRequest: Request with the aggregated tensor
+        """
+
         # Record clients waiting
         self.record_client_waiting(context)
 
@@ -103,29 +135,28 @@ class FederatedServer(federated_pb2_grpc.FederationServicer):
         # Calculate average
         clients_tensors = [
             client.tensor for client in self.federation.federation_clients]
-        print(clients_tensors[0])
-        print(clients_tensors[1])
-        print(clients_tensors[0].shape)
-        print(clients_tensors[1].shape)
         average_tensor = np.mean(np.stack(clients_tensors), axis=0)
         print("The average tensor is: ", average_tensor)
 
         # Serialize tensor
         content_bytes = average_tensor.tobytes()
         size = federated_pb2.TensorShape()
-        size.dim.extend(
-            [federated_pb2.TensorShape.Dim(size=average_tensor.shape[0], name="dim1")])
-        #size.dim.extend(
-        #    [federated_pb2.TensorShape.Dim(size=average_tensor.shape[0], name="dim1"),
-        #     federated_pb2.TensorShape.Dim(size=average_tensor.shape[1], name="dim2")])
+        num_dims = len(average_tensor.shape)
+        for i in np.arange(num_dims):
+            name = "dim" + str(i)
+            size.dim.extend(
+                [federated_pb2.TensorShape.Dim(size=average_tensor.shape[i], name=name)])
 
-        client_to_repond = federation_client.FederationClient.get_pos_by_key(context.peer(),
-                                                                             self.federation.federation_clients)
+        # Get the client to sent the update to based on the context of the gRPC channel
+        client_to_repond = \
+         federation_client.FederationClient.get_pos_by_key(context.peer(),
+                                                           self.federation.federation_clients)
         # Create request
-        update_name = "Update for client with ID " + str(self.federation.federation_clients[client_to_repond].id) + \
-                      " for the iteration " + \
-            str(
-                self.federation.federation_clients[client_to_repond].current_iter)
+        update_name = "Update for client with ID " + \
+         str(self.federation.federation_clients[client_to_repond].id) + \
+         " for the iteration " + \
+         str(self.federation.federation_clients[client_to_repond].current_iter)
+
         data = federated_pb2.Update(tensor_name=update_name,
                                     tensor_shape=size, tensor_content=content_bytes)
         # Send update
--- utils/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def get_type_from_string(str_dtype):
-    # @ TODO: Make match-case when Pytorch works with Python 3.10
-    if str_dtype == "<class 'numpy.float32'>":
-        dytpe = np.float32
-    elif str_dtype == "<class 'numpy.float64'>":
-        dytpe = np.float64
-    return dytpe
